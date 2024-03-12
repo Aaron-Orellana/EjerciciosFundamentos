f1bd39b6b5752b99cf6c7bedf7c129e5e66c9112
Aaron-Orellana <amorellanasaldias@gmail.com>
2024-03-12 14:32:11 -0300

    add: Styles

--- Ejercicio2a.py
+++ Ejercicio2a.py
@@ -4,11 +4,11 @@ if casa >= 0:
     sueldo = int(input("Ingrese el sueldo: "))
     if sueldo >= 80000:
         pie = round(casa * 0.15, 2)
-        cuotas = round(((casa * 0.85)/10)/12, 3)
+        cuotas = round((casa * 0.85)/(10*12), 3)
         print ("El pie a pagar es de: $" +str(pie))
         print ("El valor de cuotas durante 10 años es de: $"+ str(cuotas))
     if sueldo >= 0 and sueldo < 80000:
         pie = round(casa * 0.3, 2)
-        cuotas = round(((casa * 0.7)/7)/12, 3)
+        cuotas = round((casa * 0.7)/(7*12), 3)
         print ("El pie a pagar es de: $"+str(pie))
         print ("El valor de cuotas durante 10 años es de: $"+str(cuotas))
